125a850cdefcfb4b0236cb45b5f50748c2f71d72
Linda Pedroza <lsandoe@elp.rr.com>
2013-12-22 09:35:01 -0700

    fixed php problems and finished sql string

--- possibleDiseases.php
+++ possibleDiseases.php
@@ -1,5 +1,24 @@
 <?php
 
-echo"here goes";
+echo"here goes <hr><br>";
+//print_r ($checkboxes);
 
+$checkboxes = $_POST['symptoms'];
+
+$criteria = Array();
+
+foreach ($_POST['symptoms'] as $i)
+	$criteria[] = $i . ' = 1';
+	
+$sql = "SELECT * FROM main";
+if (count($criteria))
+	$sql .= " WHERE " . implode(' AND ', $criteria);
+	
+$sql = $sql . ";"; 
+
+echo $sql;
+	
 ?>
+
+
+
